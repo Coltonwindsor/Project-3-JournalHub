c08c874c2faffb7930c16da0143ea14d2cdd5a81
Colton Windsor <Coltonwindsor@yahoo.com>
2020-01-15 20:56:38 -0500

    initial commit

---
+++ client/build/precache-manifest.caa3d305b98a5a4212cba2320a636256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ab2aaab9b3fad0a18b23cd9aa536d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "394b36259610eb27e096",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "394b36259610eb27e096",
+    "url": "/static/js/main.4c99c090.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
