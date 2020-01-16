274ff7ed06bb6a9b96550de227c9c3cf94b6f3e2
Colton Windsor <Coltonwindsor@yahoo.com>
2020-01-16 16:48:58 -0500

    home page links to individual journals

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class HomePage extends Component {
     render() {
         return (
             <div>
                 <h1>Hello from the home page</h1>
+                <div><Link to='/general'>General Thoughts</Link></div>
+                <div><Link to='/food'>Food Journal</Link></div>
+                <div><Link to='/dream'>Dream Journal</Link></div>
             </div>
         )
     }
