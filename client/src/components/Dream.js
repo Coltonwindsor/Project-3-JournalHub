1ce4a1cf8bfb5c8e6e03a88c706f2e6372ee3994
Colton Windsor <Coltonwindsor@yahoo.com>
2020-01-17 09:38:26 -0500

    set up dream and single dream component

--- client/src/components/Dream.js
+++ client/src/components/Dream.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class Dream extends Component {
+    state = {
+        dreams: [],
+        newDream: {
+            name: '',
+            description: ''
+        }
+    }
+    componentDidMount() {
+        axios.get('/api/dream')
+            .then((res) => {
+                this.setState({ dreams: res.data })
+            })
+    }
+    reloadDreamList() {
+        axios.get('/api/dream')
+            .then((res) => {
+                this.setState({ dreams: res.data })
+            })
+    }
+    onChange = (evt) => {
+        const value = evt.target.value
+        const name = evt.target.name
+        const copyOfState = { ...this.state }
+        copyOfState.newDream[name] = value
+        this.setState(copyOfState)
+    }
+    onSubmit = (evt) => {
+        evt.preventDefault()
+        axios.post('/api/dream', this.state.newDream)
+            .then(() => {
+                this.reloadDreamList()
+                const copyOfState = { ...this.state }
+                copyOfState.newDream = {
+                    name: '',
+                    description: ''
+                }
+                this.setState(copyOfState)
+            })
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello from Dream Journal</h1>
+                <h1>Dream Journal</h1>
+                <form onSubmit={this.onSubmit}>
+                    <input
+                        type='text'
+                        placeholder='name'
+                        name='name'
+                        onChange={this.onChange}
+                        vlaue={this.state.newDream.name}
+                    >
+                    </input>
+                    <input
+                        type='text'
+                        placeholder='name'
+                        name='description'
+                        onChange={this.onChange}
+                        vlaue={this.state.newDream.description}
+                    >
+                    </input>
+                    <input type="submit" vlaue="Create a Dream"></input>
+                </form>
+                {this.state.dreams.map((dream) => {
+                    return (<Link to={`/${dream._id}`}>
+                        <div>{dream.name}</div>
+                    </Link>)
+                })}
             </div>
         )
     }
--- client/src/components/SingleDream.js
+++ client/src/components/SingleDream.js
@@ -1,10 +1,68 @@
 import React, { Component } from 'react'
+import axios from "axios"
+import { Redirect } from 'react-router-dom'
+
 
 export default class SingleDream extends Component {
+    state = {
+        dream: {
+            name: '',
+            description: ''
+        },
+        redirect: false
+    }
+    componentDidMount() {
+        axios.get(`/api/dream/${this.props.match.params.dreamId}`)
+            .then((res) => {
+                this.setState({ dream: res.data })
+            })
+    }
+    onChange = (evt) => {
+        const value = evt.target.value
+        const name = evt.target.name
+        const copyOfState = { ...this.state }
+        copyOfState.dream[name] = value
+        this.setState(copyOfState)
+    }
+    onSubmit = (evt) => {
+        evt.preventDefault()
+        axios.put(`/api/dream/${this.props.match.params.dreamId}`, this.state.dream)
+            .then(() => {
+                this.setState({ redirect: true })
+            })
+    }
+    deleteDream = () => {
+        axios.delete(`api/dream/${this.props.match.params.dreamId}`)
+            .then(() => {
+                this.setState({ redirect: true })
+            })
+    }
+
     render() {
         return (
             <div>
-
+                {this.state.redirect === true ? <Redirect to='/' /> : null}
+                <h1>Single Dream Page </h1>
+                <div>{this.state.dream.name}</div>
+                <div>{this.state.dream.description}</div>
+                <form onSubmit={this.onSubmit}>
+                    <input onChange={this.OnChange}
+                        type="text"
+                        name='name'
+                        placeholder='name'
+                        value={this.state.dream.name}>
+                    </input>
+                    <input onChange={this.OnChange}
+                        type="text"
+                        name='description'
+                        placeholder='description'
+                        value={this.state.dream.description}>
+                    </input>
+                    <input type="submit"
+                        value="Update Dream">
+                    </input>
+                </form>
+                <button onClick={this.deleteDream}>Delete Dream</button>
             </div>
         )
     }
