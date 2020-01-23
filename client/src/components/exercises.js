87efa6d2049a6dd7edc4aef97faebbe9d15cc91d
Colton Windsor <Coltonwindsor@yahoo.com>
2020-01-23 09:54:59 -0500

    added exercises

--- client/src/components/exercises.js
+++ client/src/components/exercises.js
@@ -1,44 +1,62 @@
 export default [
     {
-        name: 'exercise1',
-        description: 'exerciseDescription',
-        pointValue: 11,
+        name: 'Pull Ups',
+        description: '3 sets of 5',
+        pointValue: 10,
         isComplete: false
     },
     {
-        name: 'exercise2',
-        description: 'exerciseDescription',
-        pointValue: 22,
+        name: 'Pull Ups',
+        description: '5 sets of 5',
+        pointValue: 20,
         isComplete: false
     },
     {
-        name: 'exercise3',
-        description: 'exerciseDescription',
-        pointValue: 33,
+        name: 'Sit Ups',
+        description: '3 sets of 10',
+        pointValue: 10,
         isComplete: false
     },
     {
-        name: 'exercise4',
-        description: 'exerciseDescription',
-        pointValue: 44,
+        name: 'Sit Ups',
+        description: '3 sets of 20',
+        pointValue: 20,
         isComplete: false
     },
     {
-        name: 'exercise4',
-        description: 'exerciseDescription',
-        pointValue: 44,
+        name: 'Jogging',
+        description: '1 Mile',
+        pointValue: 10,
         isComplete: false
     },
     {
-        name: 'exercise4',
-        description: 'exerciseDescription',
-        pointValue: 44,
+        name: 'Jogging',
+        description: '2 Miles',
+        pointValue: 20,
         isComplete: false
     },
     {
-        name: 'exercise4',
-        description: 'exerciseDescription',
-        pointValue: 44,
+        name: 'Jogging',
+        description: '3 Miles',
+        pointValue: 30,
+        isComplete: false
+    },
+    {
+        name: 'Push Ups',
+        description: '3 sets of 10',
+        pointValue: 10,
+        isComplete: false
+    },
+    {
+        name: 'Push Ups',
+        description: '3 sets of 20',
+        pointValue: 20,
+        isComplete: false
+    },
+    {
+        name: 'Push Ups',
+        description: '5 sets of 25',
+        pointValue: 30,
         isComplete: false
     }
 ]
